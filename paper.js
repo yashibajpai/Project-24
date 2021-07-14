fdcf4e79b0a94d5e3cce76b7ae55012125dba5b5
yashibajpai <71567118+yashibajpai@users.noreply.github.com>
2021-07-14 12:51:41 -0400

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,21 @@
+class Paper {
+    constructor (x,y,r){
+        var options={
+            isStatic: false,
+            restitution: 0,
+            friction: 0.5,
+            density: 1.2, 
+        } 
+        this.body =  Bodies.circle(x,y,r,options)
+        this.r = r 
+        World.add(world,this.body)
+    }
+    display () {
+     fill("white")
+     ellipseMode(RADIUS)
+     ellipse(this.body.position.x,this.body.position.y,this.r,this.r)
+
+
+
+    }
+}
